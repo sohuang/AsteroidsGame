b84d4f372c4a2b3b0925e7454e4e708da16a152c
sohuang <sophiexhuang@gmail.com>
2016-12-06 09:57:31 -0800

    more bg

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 function init() {
-  var bgpics = ["bg1", "bg2", "bg3", "bg4"];
-  var someimage = bgpics[Math.floor(Math.random() * 4)];
-  console.log(someimage);
+  // var bgpics = ["bg1", "bg2", "bg3", "bg4"];
+  // var someimage = bgpics[Math.floor(Math.random() * 4)];
+  // console.log(someimage);
 
   // document.body.style.background = 'url('+someimage+'.png) no-repeat center center'
-  document.body.style.backgroundImage = "url(images/" + someimage + ".png)";
+  document.body.style.backgroundImage = "url(images/bg" + (Math.floor(Math.random() * 7)) + ".png)";
   document.body.style.color = "#FFFFFF";
 }
 window.onload = init;
